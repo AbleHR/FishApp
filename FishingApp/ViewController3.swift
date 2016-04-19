77625b77fadea154c7e6694f922b5abb952bf3bd
Greinke, Matthew M (grein005) <grein005@maclab-85.uwp.edu>
2016-04-19 16:19:52 -0500

    Made View controllers for the two other views

---
+++ FishingApp/ViewController3.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController3.swift
+//  FishingApp
+//
+//  Created by Greinke, Matthew M (grein005) on 4/19/16.
+//  Copyright © 2016 uwp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ViewController3: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
