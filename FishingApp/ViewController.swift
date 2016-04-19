5cfbea89f04dfd0c9062166d09893d58a1067abf
Rouse, Able H (rouse013) <rouse013@maclab-82.uwp.edu>
2016-04-19 16:27:41 -0500

    Moved map related code to view controller 2

--- FishingApp/ViewController.swift
+++ FishingApp/ViewController.swift
@@ -7,20 +7,17 @@
 //
 
 import UIKit
-import MapKit
+
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var mapView: MKMapView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        /// show users location
-        mapView.showsUserLocation = true
-        
-        mapView.mapType = MKMapType.Hybrid
+
         
         print("start")
         
@@ -31,12 +28,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func getLongPressCoordinates(sender: UILongPressGestureRecognizer) {
-        if sender.state != UIGestureRecognizerState.Began { return }
-        let touchLocation = sender.locationInView(mapView)
-        let locationCoordinate = mapView.convertPoint(touchLocation, toCoordinateFromView: mapView)
-        print("Tapped at lat: \(locationCoordinate.latitude) long: \(locationCoordinate.longitude)")
-    }
+
 
     
     //test 1
